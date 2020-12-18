e0718bbb90802782488289ea440986c44a2e9bed
Thien Nguyen <tuanthienbk@gmail.com>
2020-12-17 21:12:34 -0700

    got locals

--- clox/common.h
+++ clox/common.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define DEBUG_TRACE_EXECUTION
-#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXECUTION
+//#define DEBUG_PRINT_CODE
+#define UINT8_COUNT (UINT8_MAX + 1)
--- clox/debug.cpp
+++ clox/debug.cpp
@@ -17,6 +17,13 @@ static int simpleInstruction(const char* name, int offset)
     return offset + 1;
 }
 
+static int byteInstruction(const char* name, Chunk* chunk, int offset)
+{
+    uint8_t slot = chunk->code[offset + 1];
+    printf("%-16s %4d\n", name, slot);
+    return offset + 2;
+}
+
 static int constantInstruction(const char* name, Chunk* chunk, int offset)
 {
     uint8_t constant = chunk->code[offset + 1];
@@ -73,7 +80,11 @@ int disassembleInstruction(Chunk* chunk, int offset)
         case OP_GET_GLOBAL:
             return constantInstruction("OP_GET_GLOBAL", chunk, offset);
         case OP_SET_GLOBAL:
-              return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+            return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk, offset);
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
